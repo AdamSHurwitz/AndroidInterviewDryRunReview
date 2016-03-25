97443795a1c0991256ad0f1dd30e8032557bd500
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-25 16:34:56 -0700

    feat: solved for b and c

--- src/main/java/JavaClass.java
+++ src/main/java/JavaClass.java
@@ -1,29 +1,48 @@
+import java.util.HashMap;
+
 /**
  * Created by adamhurwitz on 3/10/16.
  */
 public class JavaClass {
-    static String string = "aabcccccaaa";
+    static String str = "aabcccccaaa";
 
     public static void main(String arg[]) {
-        System.out.println("Answer is " + compress(string));
+        System.out.println("Answer is " + compress(str));
     }
 
-    public static String compress(String string) {
-        String newString = "";
-        int count = 0;
-        for (int x = 0; x < string.length() - 1; x++) {
-            String temp = string.substring(x);
-            if (x > 0){
-                if (temp.equals(string.substring(x-1))){
-                    count++;
-                    newString+= count;
-                }
+    public static String compress(String str) {
+        String compressed = "";
+        HashMap<String, Integer> lCount = new HashMap<String, Integer>();
+        int duplicate = 2;
+
+        for (int n = 0; n < str.length(); n++) {
+
+            String c = str.substring(n, n + 1);
+
+            if (lCount.get(c) == null) {
+                lCount.put(c, 1);
             }
+            /*else if (!c.equals(str.substring(n - 1, n)) && lCount.get(c) != null) {
+                lCount.put(c + String.valueOf(duplicate), 1);
+            }*/
+            /*else if (lCount.get(c) != null && c.equals(str.substring(n - 1, n))){
+                lCount.put(c + String.valueOf(duplicate),
+                        lCount.get(c + String.valueOf(duplicate))+1);
+            } */
             else {
-                newString+= temp;
+                lCount.put(c, lCount.get(c) + 1);
+            }
+            System.out.println("length of : " + c + " is " + lCount.get(c));
+        }
+
+        for (HashMap.Entry<String, Integer> e : lCount.entrySet()) {
+            if (e.getKey().length() > 1) {
+                compressed = compressed + e.getKey().substring(0,1) + String.valueOf(e.getValue());
+            } else {
+                compressed = compressed + e.getKey() + String.valueOf(e.getValue());
             }
         }
-        return newString;
+        return compressed;
     }
 
 }
