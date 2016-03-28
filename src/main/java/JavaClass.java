5a69525d2c4c8cc60d5cad00e646c16098e26dce
adamhurwitz <adamscotthurwitz@gmail.com>
2016-03-28 16:34:28 -0700

    feat: solved

--- src/main/java/JavaClass.java
+++ src/main/java/JavaClass.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 /**
  * Created by adamhurwitz on 3/10/16.
  */
@@ -12,33 +10,16 @@ public class JavaClass {
 
     public static String compress(String str) {
         String compressed = "";
-        HashMap<String, Integer> lCount = new HashMap<String, Integer>();
-        int dup = 1;
-
-        for (int n = 0; n < str.length(); n++) {
-            String c = str.substring(n, n + 1);
-
-            if (lCount.get(c) == null) {
-                lCount.put(c, 1);
+        int counter = 1;
+        for (int n = 1; n <= str.length(); n++){
+            if (n == str.length()){
+                compressed = compressed + str.substring(n-1,n) + counter;
             }
-            /*else if (!c.equals(str.substring(n - 1, n)) && lCount.get(c) != null) {
-                lCount.put(c + String.valueOf(duplicate), 1);
-            }*/
-            /*else if (lCount.get(c) != null && c.equals(str.substring(n - 1, n))){
-                lCount.put(c + String.valueOf(duplicate),
-                        lCount.get(c + String.valueOf(duplicate))+1);
-            } */
-            else {
-                lCount.put(c, lCount.get(c) + 1);
-            }
-            System.out.println("length of : " + c + " is " + lCount.get(c));
-        }
-
-        for (HashMap.Entry<String, Integer> e : lCount.entrySet()) {
-            if (e.getKey().length() > 1) {
-                compressed = compressed + e.getKey().substring(0,1) + String.valueOf(e.getValue());
+            else if (!str.substring(n,n+1).equals(str.substring(n-1,n))){
+                compressed = compressed + str.substring(n-1,n) + counter;
+                counter = 1;
             } else {
-                compressed = compressed + e.getKey() + String.valueOf(e.getValue());
+                counter++;
             }
         }
         return compressed;
